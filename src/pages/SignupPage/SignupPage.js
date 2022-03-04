9364300ee2c62da67fc390c7419d769649f66b87
Jo Faria <joana.adaodefaria@gmail.com>
2022-03-04 18:34:47 +0000

    improve sign up flow in master

--- src/pages/SignupPage/SignupPage.js
+++ src/pages/SignupPage/SignupPage.js
@@ -1,13 +1,18 @@
 // import axios from "axios";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import authService from "../../services/auth.service";
+import { AuthContext } from "../../context/auth.context";
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_SERVER_URL;
 
 function SignupPage(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
   const [phone, setPhone] = useState("");
+  const { logInUser } = useContext(AuthContext);
 
   const [errorMessage, setErrorMessage] = useState(undefined);
 
@@ -23,14 +28,19 @@ function SignupPage(props) {
       e.preventDefault();
       // Create an object representing the request body
       const requestBody = { email, password, username, phone };
-
       await authService.signup(requestBody);
 
-      // If the request is successful navigate to login page
-      navigate("/login");
+      const loginInfo = { email, password };
+
+      const response = await authService.login(loginInfo);
+
+      const token = response.data.authToken;
+      logInUser(token);
+
+      navigate("/");
     } catch (error) {
       // If the request resolves with an error, set the error message in the state
-      setErrorMessage("Please enter valid fields.");
+      setErrorMessage(error.response.data.message);
     }
   };
 
@@ -65,12 +75,7 @@ function SignupPage(props) {
           />
 
           <label>Phone:</label>
-          <input
-            type="number"
-            name="phone"
-            value={phone}
-            onChange={handlePhone}
-          />
+          <input type="tel" name="phone" value={phone} onChange={handlePhone} />
 
           <button className="my-btn-white" type="submit">
             Sign Up
